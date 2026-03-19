50413366c05782f231429bfe059554a6e4637615
Proteios1998 <zp88@cornell.edu>
2026-03-19 17:14:33 -0400

    update demo plots

---
+++ demo/multi_panel_demo.py
@@ -0,0 +1,18 @@
+import plotkit as pk
+import numpy as np
+import matplotlib.pyplot as plt
+
+pk.auto.apply_auto_theme("scatter", font="Arial")
+
+fig, axes = pk.themes.setup_multi_panel(1, 2, figsize=(10,4))
+
+x = np.random.randn(200)
+axes[0].scatter(x, np.random.randn(200))
+axes[0].set_title("Panel A")
+axes[1].hist(x, bins=20)
+axes[1].set_title("Panel B")
+
+pk.add_panel_labels(fig, axes)
+
+fig.tight_layout()
+plt.savefig("multi_panel_demo.pdf")
